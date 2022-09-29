e2c45de7b159c7732094d2342dcff927989fb8e8
frankcl <frank.lichen@gmail.com>
2022-09-29 15:54:15 +0800

    add ShortKeyBuilder impl and testcase

--- weapon-base/src/main/java/com/manong/weapon/base/util/ShortKeyBuilder.java
+++ weapon-base/src/main/java/com/manong/weapon/base/util/ShortKeyBuilder.java
@@ -1,18 +1,15 @@
 package com.manong.weapon.base.util;
 
 import org.apache.commons.codec.digest.DigestUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * 短key构建器
+ * 短key编码器
+ * 采用数字0-9，小写字母a-z，大写字母A-Z进行编码
  *
  * @author frankcl
  * @date 2022-09-28 15:01:28
  */
 public class ShortKeyBuilder {
-
-    private final static Logger logger = LoggerFactory.getLogger(ShortKeyBuilder.class);
 
     /* 编码字符集合 */
     private final static char[] ENCODE_CHARS = new char[] {
@@ -25,21 +22,20 @@ public class ShortKeyBuilder {
             'Y', 'Z'
     };
     private final static long ONE_BYTE_MASK = 0x3D;
-    private final static long ONE_BYTE_UNMASK = 0xC4;
     private final static long TWO_BYTE_MASK = 0x3FFFFFFF;
 
     /**
-     * 针对输入文本构建简短key
+     * 将输入文本进行编码，生成key
      *
      * @param text 输入文本
-     * @return 简短key
+     * @return 编码key
      */
     public static String build(String text) {
         String md5 = DigestUtils.md5Hex(text == null ? "" : text);
         StringBuffer buffer = new StringBuffer();
-        int[] codes = innerBuild(md5);
+        int[] codes = buildCodes(md5);
         for (int code : codes) buffer.append(ENCODE_CHARS[code % ENCODE_CHARS.length]);
-        codes = innerBuild(new StringBuffer(md5).reverse().toString());
+        codes = buildCodes(new StringBuffer(md5).reverse().toString());
         for (int code : codes) buffer.append(ENCODE_CHARS[code % ENCODE_CHARS.length]);
         return buffer.toString();
     }
@@ -50,7 +46,7 @@ public class ShortKeyBuilder {
      * @param md5 字符串MD5
      * @return 编码表
      */
-    private static int[] innerBuild(String md5) {
+    private static int[] buildCodes(String md5) {
         int codes[] = new int[6];
         for (int i = 0; i < 4; i++) {
             String segment = md5.substring(i * 8, (i + 1) * 8);
