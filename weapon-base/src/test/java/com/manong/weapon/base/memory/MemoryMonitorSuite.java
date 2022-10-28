e29a37601309fabfe223ced14310770fd5c58d4b
frankcl <frank.lichen@gmail.com>
2022-10-28 11:46:03 +0800

    add MemoryMonitor impl and testcase

---
+++ weapon-base/src/test/java/com/manong/weapon/base/memory/MemoryMonitorSuite.java
@@ -0,0 +1,18 @@
+package com.manong.weapon.base.memory;
+
+import org.junit.Test;
+
+/**
+ * @author frankcl
+ * @date 2020-08-03 16:45:33
+ */
+public class MemoryMonitorSuite {
+
+    @Test
+    public void testForceFullGC() {
+        MemoryMonitor memoryMonitor = new MemoryMonitor(new MemoryMonitorConfig());
+        System.out.println("before: " + memoryMonitor.getCurrentMemory());
+        memoryMonitor.forceFullGC();
+        System.out.println("after: " + memoryMonitor.getCurrentMemory());
+    }
+}
