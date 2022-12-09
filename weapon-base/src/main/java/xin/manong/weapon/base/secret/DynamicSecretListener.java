186d504d0b833a660cc317789e2adfd26f06616a
frankcl <frank.lichen@gmail.com>
2022-12-09 14:53:49 +0800

    adjust package structure

--- weapon-base/src/main/java/xin/manong/weapon/base/secret/DynamicSecretListener.java
+++ weapon-base/src/main/java/xin/manong/weapon/base/secret/DynamicSecretListener.java
@@ -1,5 +1,8 @@
 package xin.manong.weapon.base.secret;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 动态秘钥监听器
  *
@@ -7,6 +10,15 @@ package xin.manong.weapon.base.secret;
  * @date 2022-12-09 10:38:26
  */
 public interface DynamicSecretListener {
+
+    Logger logger = LoggerFactory.getLogger(DynamicSecretListener.class);
+
+    /**
+     * 启动监听器
+     */
+    default void start() {
+        logger.info("dynamic secret listener[{}] has been started", this.getClass().getName());
+    }
 
     /**
      * 订阅和处理秘钥变更
