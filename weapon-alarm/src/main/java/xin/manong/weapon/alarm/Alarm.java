1edb6be82b731207da4ee6c11ab8e012ca035831
frankcl <frank.lichen@gmail.com>
2022-12-10 14:53:04 +0800

    add phone channel for alarm

--- weapon-alarm/src/main/java/xin/manong/weapon/alarm/Alarm.java
+++ weapon-alarm/src/main/java/xin/manong/weapon/alarm/Alarm.java
@@ -34,4 +34,59 @@ public class Alarm {
         this(status);
         this.content = content;
     }
+
+    /**
+     * 设置应用名
+     *
+     * @param appName 应用名
+     * @return 报警信息
+     */
+    public Alarm setAppName(String appName) {
+        this.appName = appName;
+        return this;
+    }
+
+    /**
+     * 设置报警状态
+     *
+     * @param status 报警状态
+     * @return 报警信息
+     */
+    public Alarm setStatus(AlarmStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    /**
+     * 设置报警标题
+     *
+     * @param title 报警标题
+     * @return 报警信息
+     */
+    public Alarm setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    /**
+     * 设置报警内容
+     *
+     * @param content 报警内容
+     * @return 报警信息
+     */
+    public Alarm setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+    /**
+     * 设置报警时间
+     *
+     * @param time 报警时间
+     * @return 报警信息
+     */
+    public Alarm setTime(Long time) {
+        this.time = time;
+        return this;
+    }
 }
