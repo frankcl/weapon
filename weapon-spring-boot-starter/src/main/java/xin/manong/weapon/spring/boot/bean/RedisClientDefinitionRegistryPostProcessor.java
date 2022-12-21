a8ab2623ce4adaa430cc2270820f231bed1a5971
frankcl <frank.lichen@gmail.com>
2022-12-21 11:26:45 +0800

    support RedisClient for spring boot starter

--- weapon-spring-boot-starter/src/main/java/xin/manong/weapon/spring/boot/bean/RedisClientDefinitionRegistryPostProcessor.java
+++ weapon-spring-boot-starter/src/main/java/xin/manong/weapon/spring/boot/bean/RedisClientDefinitionRegistryPostProcessor.java
@@ -25,7 +25,7 @@ public class RedisClientDefinitionRegistryPostProcessor extends AliyunBeanDefini
 
     private final static Logger logger = LoggerFactory.getLogger(RedisClientDefinitionRegistryPostProcessor.class);
 
-    private final static String BINDING_KEY = "weapon.aliyun.redis";
+    private final static String BINDING_KEY = "weapon.common.redis";
     private final static String KEY_MODE = "mode";
 
     @Override
