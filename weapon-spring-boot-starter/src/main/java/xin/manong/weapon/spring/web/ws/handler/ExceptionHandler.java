9fe98e9f3f42ab1b14b5d609ccb0137f4aa53275
frankcl <frank.lichen@gmail.com>
2023-07-14 14:56:22 +0800

    fix bug: cause exception is not correct for ExceptionHandler

--- weapon-spring-boot-starter/src/main/java/xin/manong/weapon/spring/web/ws/handler/ExceptionHandler.java
+++ weapon-spring-boot-starter/src/main/java/xin/manong/weapon/spring/web/ws/handler/ExceptionHandler.java
@@ -1,6 +1,5 @@
 package xin.manong.weapon.spring.web.ws.handler;
 
-import org.apache.commons.lang3.StringUtils;
 import xin.manong.weapon.spring.web.WebResponse;
 
 import javax.ws.rs.WebApplicationException;
@@ -19,15 +18,16 @@ public class ExceptionHandler implements ExceptionMapper<Exception> {
 
     @Override
     public Response toResponse(Exception e) {
+        if (e instanceof UndeclaredThrowableException) {
+            Exception cause = (Exception) (((UndeclaredThrowableException) e).getUndeclaredThrowable().getCause());
+            if (cause != null) e = cause;
+        }
         if (e instanceof WebApplicationException) {
             WebApplicationException webApplicationException = (WebApplicationException) e;
             return Response.status(Response.Status.OK.getStatusCode()).type(MediaType.APPLICATION_JSON).entity(
                     WebResponse.buildError(webApplicationException.getResponse().getStatus(), e.getMessage())).build();
         }
         String message = e.getMessage();
-        if (StringUtils.isEmpty(message) && e instanceof UndeclaredThrowableException) {
-            message = ((UndeclaredThrowableException) e).getUndeclaredThrowable().getMessage();
-        }
         return Response.status(Response.Status.OK.getStatusCode()).type(MediaType.APPLICATION_JSON).entity(
                 WebResponse.buildError(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), message)).build();
     }
