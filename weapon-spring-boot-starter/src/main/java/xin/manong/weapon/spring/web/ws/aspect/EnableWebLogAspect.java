74e9a32e05cc02ba8294c57073e0b46251a9890b
frankcl <frank.lichen@gmail.com>
2023-10-17 14:48:01 +0800

    add Import annotation to import WebLogAspect component

--- weapon-spring-boot-starter/src/main/java/xin/manong/weapon/spring/web/ws/aspect/EnableWebLogAspect.java
+++ weapon-spring-boot-starter/src/main/java/xin/manong/weapon/spring/web/ws/aspect/EnableWebLogAspect.java
@@ -1,5 +1,7 @@
 package xin.manong.weapon.spring.web.ws.aspect;
 
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -13,6 +15,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@Import({ WebLogAspect.class })
 public @interface EnableWebLogAspect {
 
     boolean commitResponse() default true;
