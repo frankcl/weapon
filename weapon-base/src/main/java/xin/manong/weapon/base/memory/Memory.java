0f08e75769a9bbfa1054284a65ece417169e260e
frankcl <frank.lichen@gmail.com>
2023-11-01 16:38:55 +0800

    support json for Memory object

--- weapon-base/src/main/java/xin/manong/weapon/base/memory/Memory.java
+++ weapon-base/src/main/java/xin/manong/weapon/base/memory/Memory.java
@@ -1,16 +1,24 @@
 package xin.manong.weapon.base.memory;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * JVM内存
  *
  * @author frankcl
  * @date 2022-10-28 11:24:37
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Memory {
 
+    @JsonProperty("use_memory")
     public long useMemory = 0L;
+    @JsonProperty("free_memory")
     public long freeMemory = 0L;
+    @JsonProperty("heap_memory")
     public long heapMemory = 0L;
+    @JsonProperty("max_heap_memory")
     public long maxHeapMemory = 0L;
 
     @Override
