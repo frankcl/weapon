e1f1d77f2d3b88468e4a792ce9447491f3a99eda
frankcl <frank.lichen@gmail.com>
2025-09-25 10:02:45 +0800

    add exception log in ExceptionHandler

--- weapon-jersey/src/main/java/xin/manong/weapon/jersey/handler/ExceptionHandler.java
+++ weapon-jersey/src/main/java/xin/manong/weapon/jersey/handler/ExceptionHandler.java
@@ -4,6 +4,8 @@ import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import xin.manong.weapon.jersey.WebResponse;
 
 import java.lang.reflect.UndeclaredThrowableException;
@@ -16,17 +18,19 @@ import java.lang.reflect.UndeclaredThrowableException;
  */
 public class ExceptionHandler implements ExceptionMapper<Exception> {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
+
     @Override
     public Response toResponse(Exception e) {
         if (e instanceof UndeclaredThrowableException) {
             Exception cause = (Exception) (((UndeclaredThrowableException) e).getUndeclaredThrowable().getCause());
             if (cause != null) e = cause;
         }
-        if (e instanceof WebApplicationException) {
-            WebApplicationException webApplicationException = (WebApplicationException) e;
+        if (e instanceof WebApplicationException webApplicationException) {
             return Response.status(Response.Status.OK.getStatusCode()).type(MediaType.APPLICATION_JSON).entity(
                     WebResponse.buildError(webApplicationException.getResponse().getStatus(), e.getMessage())).build();
         }
+        logger.error(e.getMessage(), e);
         String message = e.getMessage();
         return Response.status(Response.Status.OK.getStatusCode()).type(MediaType.APPLICATION_JSON).entity(
                 WebResponse.buildError(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), message)).build();
